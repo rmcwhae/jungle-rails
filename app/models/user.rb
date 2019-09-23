a31b89f4311a3e8cac6b5c61508047ba1cff15a7
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-23 18:42:22 +0000

    Adds working test for unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password_digest, presence: true, length: { minimum: 1 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
@@ -7,10 +9,16 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
 
-    it 'Error when password and password confirmation do not match' do
+    it 'Password and password confirmation must match' do
       @user = User.new(first_name: 'Harry', last_name: 'Potter', password: 'test', password_confirmation: 'stuff', email: 'test@test.com')
       expect(@user).to_not be_valid
       expect(@user.errors.full_messages.first).to eq "Password confirmation doesn't match Password"
+    end
+
+    it 'Email must be unique' do
+      @user1 = User.create(first_name: 'Harry', last_name: 'Potter', password: 'test', password_confirmation: 'test', email: 'test@test.com')
+      @user2 = User.new(first_name: 'Ron', last_name: 'Weasley', password: 'stuff', password_confirmation: 'stuff', email: 'TEST@TEST.com')
+      expect(@user2).to_not be_valid
     end
 
     it 'Email, first and last names must be provided' do
@@ -20,6 +28,5 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages.second).to eq "Last name can't be blank"
       expect(@user.errors.full_messages.third).to eq "Email can't be blank"
     end
-
   end
 end
